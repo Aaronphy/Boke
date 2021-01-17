fdbbfbc53286ffe3da906afaa7a8818523f1cf5e
Aaronphy <aaronphy@AaronphydeMacBook-Pro.local>
2021-01-17 20:13:19 +0800

    add default components

---
+++ src/components/DefaultFallback.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {ErrorComponentProps} from 'types/index';
+
+export default function DefaultFallBack({name,error}:ErrorComponentProps){
+    return (
+        <div>
+            <div>Failed to render {name}</div>
+            <pre>{error.message}</pre>
+        </div>
+    )
+}
